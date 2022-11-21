2c97fdb08c862eadf4fa92dab836ca0b18692046
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-11-21 23:08:03 +0100

    Card Flip on Click

--- src/feature/dashboard/project-card.tsx
+++ src/feature/dashboard/project-card.tsx
@@ -1,6 +1,6 @@
 import { Box, Card, Paper, Typography } from '@mui/material';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface ProjectCardData {
   category: string;
@@ -16,9 +16,33 @@ type Props = {
 };
 
 const ProjectCard = ({ data }: Props) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const flipCard = () => {
+    setFlipped(!flipped);
+  };
+
   return (
-    <Box sx={{ minWidth: '28rem', width: '100%' }}>
-      <Card sx={{ width: '100%' }}>
+    <Box
+      sx={{
+        minWidth: '10rem',
+        maxWidth: '50rem',
+        aspectRatio: '18/13',
+        width: '100%',
+        transition: 'transform 0.8s',
+        transformStyle: 'preserve-3d',
+        transform: flipped ? 'rotateY(180deg)' : ''
+      }}
+      onClick={() => flipCard()}
+    >
+      <Card
+        className={'card-front'}
+        sx={{
+          width: '100%',
+          backfaceVisibility: 'hidden',
+          position: 'absolute'
+        }}
+      >
         <Box
           sx={{
             width: '100%',
@@ -29,6 +53,52 @@ const ProjectCard = ({ data }: Props) => {
         >
           <Image src={data.image} alt="Project Image" layout="fill" />
         </Box>
+        <Box
+          sx={{
+            height: '5rem',
+            padding: '1rem 2.5rem',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-end'
+          }}
+        >
+          <div>
+            <Typography variant="h6">{data.title}</Typography>
+            <Box>
+              {data.tags.map((tag) => (
+                <Typography
+                  key={tag}
+                  variant="caption"
+                  sx={{ whiteSpace: 'nowrap' }}
+                >
+                  {`${tag}, `}
+                </Typography>
+              ))}
+            </Box>
+          </div>
+          <div>Arrow</div>
+        </Box>
+      </Card>
+
+      <Card
+        className={'card-back'}
+        sx={{
+          width: '100%',
+          backgroundColor: 'dodgerblue',
+          color: 'white',
+          transform: 'rotateY(180deg)',
+          position: 'absolute',
+          backfaceVisibility: 'hidden'
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            height: 0,
+            paddingBottom: '56.25%',
+            position: 'relative'
+          }}
+        ></Box>
         <Box
           sx={{
             height: '5rem',
--- src/pages/dashboard/mock-data.ts
+++ src/pages/dashboard/mock-data.ts
@@ -40,25 +40,25 @@ export const MOCK_CARDS: ProjectCardData[] = [
       'Material UI',
       'CSS'
     ]
-  },
-  {
-    category: 'Sticker Maker',
-    image:
-      'https://images.pexels.com/photos/323645/pexels-photo-323645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Sticker Maker',
-    description: 'This is my Sticker Maker Website',
-    weblinks: [
-      { url: 'figma.com', type: 'figma' },
-      { url: 'website.com', type: 'website' },
-      { url: 'github.com', type: 'github' }
-    ],
-    tags: [
-      'NextJS',
-      'ReactJS',
-      'JavaScript',
-      'TailwindCSS',
-      'Material UI',
-      'CSS'
-    ]
   }
+  // {
+  //   category: 'Sticker Maker',
+  //   image:
+  //     'https://images.pexels.com/photos/323645/pexels-photo-323645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  //   title: 'Sticker Maker',
+  //   description: 'This is my Sticker Maker Website',
+  //   weblinks: [
+  //     { url: 'figma.com', type: 'figma' },
+  //     { url: 'website.com', type: 'website' },
+  //     { url: 'github.com', type: 'github' }
+  //   ],
+  //   tags: [
+  //     'NextJS',
+  //     'ReactJS',
+  //     'JavaScript',
+  //     'TailwindCSS',
+  //     'Material UI',
+  //     'CSS'
+  //   ]
+  // }
 ];
