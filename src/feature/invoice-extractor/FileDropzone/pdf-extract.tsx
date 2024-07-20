0d92a2475c28b06229ade8acb0f2aff103b02eb9
rakkidi <101603072+tlvu93@users.noreply.github.com>
2024-07-20 21:21:16 +0200

    Fix polyfill error

--- src/feature/invoice-extractor/FileDropzone/pdf-extract.tsx
+++ src/feature/invoice-extractor/FileDropzone/pdf-extract.tsx
@@ -2,21 +2,8 @@ import { DocumentInitParameters } from 'pdfjs-dist/types/src/display/api';
 
 import { pdfjs } from 'react-pdf';
 
-if (typeof Promise.withResolvers === 'undefined') {
-  if (window)
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    window.Promise.withResolvers = function () {
-      let resolve, reject;
-      const promise = new Promise((res, rej) => {
-        resolve = res;
-        reject = rej;
-      });
-      return { promise, resolve, reject };
-    };
-}
-
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+  'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url
 ).toString();
 
